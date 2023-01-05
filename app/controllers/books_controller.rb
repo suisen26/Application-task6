03a408f08576069a7c2c3f8136f5e013d64e74d8
EC2 Default User <ec2-user@ip-172-31-15-116.ap-northeast-1.compute.internal>
2023-01-05 08:25:09 +0000

    [Finish] 応用課題6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_new = Book.new
     @book_comment = BookComment.new
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -16,7 +16,7 @@ class Book < ApplicationRecord
     if method == "perfect"
       Book.where(title: content)
     elsif method == "forward"
-      Book.where("title LIKE ?",content + "%")
+      Book.where("title LIKE ?", content + "%")
     elsif method == "backward"
       Book.where("title LIKE ?", "%" + content)
     else
